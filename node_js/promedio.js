7ace1e5d7cd1bf049cae325bea88d6614c76692c
Raquel Monserrat Carlos Perez <monse@99.200-57-1.bestelclientes.com.mx>
2019-05-31 20:58:07 -0500

    ejercios clase 9

--- node_js/promedio.js
+++ node_js/promedio.js
@@ -10,14 +10,32 @@ function suma(calificaciones){
     return suma
 }
 
+function promedio(calificaciones){
+    let promedio = 0
+
+    promedio = suma(calificaciones)/calificaciones.length
+
+    return promedio
+
+}
+
+function valida(calificaciones){
+
+    //const sumacalif=suma(calificaciones)
+    //const resultado = promedio(califcaciones)
+
+    if(promedio(calificaciones) > 8){
+        console.log("Felicidades tienes la beca, tu promedio es de:  "+ promedio(calificaciones))
+    }
+    else{
+        console.log("Suerte para la proxima")
+    }
+
+}
+
+
 const calificaciones=[10,7,8]
-let promedio = 0
 
-promedio = suma(calificaciones)/calificaciones.length
+valida(calificaciones)
 
-if(promedio > 8){
-    console.log("Felicidades tienes la beca, tu promedio es de:  "+ promedio)
-}
-else{
-    console.log("Suerte para la proxima")
-}
+
